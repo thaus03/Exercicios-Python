0a597501219ffc58d6262c52dcc5ebfa8ba79ba8
Igor Carvalho <igor.carvalho@live.com>
2019-09-02 12:29:30 -0300

    Novos Arquivos

---
+++ Aula19/AulaPratica.py
@@ -0,0 +1,7 @@
+pessoas = {'nome': 'Igor', 'sexo': 'M', 'idade': 28}
+# ou pessoas = dict()
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos')
+
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
